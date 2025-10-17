0439361ab174f51043f662880d74a50ebd374a8c
eregeikechukwu <eregeikechukwu@gmail.com>
2025-10-17 13:35:59 +0100

    fixed google verification file access

--- next.config.ts
+++ next.config.ts
@@ -7,25 +7,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["agc-storage.s3.eu-north-1.amazonaws.com"],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/googlea264a916086e83a6.html",
-        destination: "/api/googlea264a916086e83a6",
-      },
-    ];
-  },
-  redirects: async () => {
-    return [
-      {
-        source:
-          "https://agc-news-nelson-erege.vercel.app/googlea264a916086e83a6.html",
-        destination:
-          "https://agc-news-nelson-erege.vercel.app/googlea264a916086e83a6.html",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
