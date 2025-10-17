c9158419e8d0b10f2980704706f5d7f222f53b18
eregeikechukwu <eregeikechukwu@gmail.com>
2025-10-17 13:16:45 +0100

    trying to fix google verificatio file path

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,18 @@ const nextConfig: NextConfig = {
         destination: "/api/googlea264a916086e83a6",
       },
     ];
-  }
+  },
+  redirects: async () => {
+    return [
+      {
+        source:
+          "https://agc-news-nelson-erege.vercel.app/googlea264a916086e83a6.html",
+        destination:
+          "https://agc-news-nelson-erege.vercel.app/googlea264a916086e83a6.html",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
